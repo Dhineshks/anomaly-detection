b735c2c9327894974a4e4117c99a3acfaa90d4ac
DhineshKs <dhineshksd1997@gmail.com>
2020-05-08 15:59:38 +0530

    function for estimating gaussian

--- func.py
+++ func.py
@@ -6,4 +6,16 @@ from numpy import genfromtxt
 def read_dataset(filepath,delimiter=','):
     return genfromtxt(filepath,delimiter=delimiter)
 
-
+def estimateGaussian(x):
+    m,n = x.shape
+    
+    #For returning mean and variance of both the columns
+    mu = np.zeros(n)
+    sigma_square = np.zeros(n)
+    
+    #mean
+    mu = (1/m) * np.sum(x,axis = 0)
+    #variance
+    sigma_square = (1/m) * np.sum((x - mu) ** 2, axis=0) 
+    
+    return mu , sigma_square
